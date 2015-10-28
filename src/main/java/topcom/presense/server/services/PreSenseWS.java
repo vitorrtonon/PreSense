a2ec24a065dde70eb5e84bd6e85e7f9499dafaa9
Amanda Ruiz <hikarikyuubichan@gmail.com>
2015-10-28 18:44:40 -0200

    Corrige lógica

--- src/main/java/topcom/presense/server/services/PreSenseWS.java
+++ src/main/java/topcom/presense/server/services/PreSenseWS.java
@@ -2,74 +2,78 @@ package topcom.presense.server.services;
 
 import topcom.presense.server.dao.*;
 import topcom.presense.server.pojo.*;
+import java.util.Calendar;
+import java.sql.Timestamp;
 
 public class PreSenseWS {
 
 	// <delete later> 
-	// Assumindo apenas comunicação de um evento e não para externo, 
-	// como CRUD de eventos [que envolve calculos de preseça], de pessoas, 
-	// beacons, managers ou sensores) -> Apenas lida com Attendance
-	// Questão: Tratando attendance como a "presença" do ER, sendo 'atende'
-	// apenas algo a ser computado após o encerramento do evento, não neste código.
-	// Questão: Distância é necessária?
-	// 
-	// TO-DO: <?> buscar Attendance ativo (recebendo evento e pessoa, buscando
-	// por um onde encerramento é NULL)
 	// TO-DO: usar lib json para leitura da entrada
 	// TO-DO: lidar com a autentificação (pra extrair evento)
-	// >> Ultimos dois envolvem alteração dos valores de campos utilizados
-	// nas buscas e instanciações
-	//
-	// Questão: Resource é a mensagem HTTP integra?
 	public void receiveInfoFromSensor(byte[] resource) {
 
-		// Trocar para uma leitura JSON?
-		//String[] res = (new String(byte)).split("[,] | [\n] | [ : ] | [\"]");
-		//int l = res.length;  
-		//if (l != 10 || l != 12) { // If a sensor message 
-		//	System.err.println("Invalid stream received");
-		//	return;
-		//}
-		//if (l == 12) // If update or in (ps: Out has 10 tokens)
-		//	double dist = Double.parseDouble(res[1]); 
-		//String[] t = res[l-3].split("[-] | [T] | [:] | [Z]"); 
+		// // < Fazer leitura Json>
+		// if (/*HTTP GET*/) {
+		// 	this.auth(/*stream*/);
+		// 	return;
+		// }
+		// // else
+		
+		// String bId = /*getMajor + getMinor + getUuid*/;
+		// String res[] = /*getTime*/.split("[-] | [T] | [:] | [Z]");
+		// String user = /*getUser*/
+		// String pass = /*getPass*/
 
-		// Convert time to Timestamp 
-		//Calendar cal = Calendar.getInstance(); 
-		//cal.set(Integer.parseInt(t[0]), Integer.parseInt(t[1]),Integer.parseInt(t[2]),
-		//		Integer.parseInt(t[3]), Integer.parseInt(t[4]), Integer.parseInt(t[5]));
-		//Timestamp t = new Timestamp(cal.getTimeInMillis());	
+		// // Convert time to Timestamp 
+		// Calendar cal = Calendar.getInstance(); 
+		// cal.set(Integer.parseInt(t[0]), Integer.parseInt(t[1]),Integer.parseInt(t[2]),
+		// 		Integer.parseInt(t[3]), Integer.parseInt(t[4]), Integer.parseInt(t[5]));
+		// Long time = cal.getTimeInMillis();
 		
-		// Retrieve event using sensor's search
-		//Event ev = EventDAO.findEventById(SensorDao.findSensorById(1).getEvent());
-		//if (ev == null) {
-		//	System.err.println("Unregistered event")
-		//	return;
-		//}
+		// // Retrieve event using sensor's search
+		// Event ev = EventDAO.findEventById(
+		// 	/*SensorDao.findSensorByName(user, pass).getEvent());*/
+		// if (ev == null) {
+		// 	System.err.println("Unregistered event")
+		// 	return;
+		// }
 		
-		// Retrieve person using beacon's search
-		//Person p = BeaconDAO.findBeaconById(res[l-7] + res[l-5] + res[l-1]).getPerson();
-		//if (p == null) {
-		//	System.err.println("Unregistered person")
-		//	return;
-		//}
+		// // Retrieve person using beacon's search
+		// Person p = BeaconDAO.findBeaconById().getPerson();
+		// if (p == null) {
+		// 	System.err.println("Unregistered person")
+		// 	return;
+		// }
 				
-		// Process rightful sensor-event type 
-		//if (res[l-9].equals("in")) { // Register new attendence for 'p' in 'event'
-		//	AttendanceDao.insert(new Attendance(ev, p, t, null));
-		//} else if (res[l-9].equals("out")) {
-			// Search for active attendence entry for 'p' in 'event,
-		//	if ((presence = /*<TO-DO>?*/AttendanceDAO.findAttendance(event, p, null)) != null)
-		//		presence.setDepartureTime(new Timestamp(System.currentTimeMillis()));
-		//	else // Else, some kind of error?
-		//		System.err.println("Presence not found. Unknown error."); 
-		//} else if (res[l-9].equals("update")) {
-		//	// Search for active attendence for 'p' in 'event', if nothing is found, 
-			// start a new attendance (probably communication loss during 'in' message)
-		//	if ((presence = /*<TO-DO>?*/AttendanceDAO.findAttendance(event, p, null)) == null)
-		//		AttendanceDao.insert(new Attendance(ev, p, t, null));
-		//} else System.err.println("Invalid stream received");
+		// // Search for Attendance, if doesn't exist, creat it
+		// Attendance att = findAttendanceByEventAndPerson(e, p);
+		// if (att == null) att = new Attendance(e, p, new Timestamp(0), new Timestamp(time));
+		// else {
+		// 	// Process rightful sensor-event type
+		// 	if (type.equals("in")) att.setLastTime(time);
+		// 	else if (type.equals("update")) {
+		// 		Long delta = time - att.getLastTime.getTime();
+		// 		att.setTotalTime(att.getTotalTime.getTime() + delta);
+		// 		att.setLastTime(new Timestamp(time));
+		// 	}
+		// 	else if (type.equals("out")) {
+		// 		continue;
+		// 	}
+		// 	else {
+		// 		System.err.println("Invalid stream received");
+		// 		return;
+		// 	}
+		// }
 		
 	}
+
+	public void auth(void)   {
+		// Verify PIN
+		// Create passcode
+		// Elaborate json response
+		// Encrypt passcode and update sql
+		// Send
+	}
+
 
 }

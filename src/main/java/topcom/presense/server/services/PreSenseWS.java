ea10e7b6c63d414d29d60ffafcde34fe22ace8a0
Amanda Ruiz <hikarikyuubichan@gmail.com>
2015-10-28 19:36:58 -0200

    Adiciona classe pojo para utilizar com gson, e atualiza WS com utilizaçaõ do gson

--- src/main/java/topcom/presense/server/services/PreSenseWS.java
+++ src/main/java/topcom/presense/server/services/PreSenseWS.java
@@ -4,6 +4,7 @@ import topcom.presense.server.dao.*;
 import topcom.presense.server.pojo.*;
 import java.util.Calendar;
 import java.sql.Timestamp;
+import com.google.gson.Gson;
 
 public class PreSenseWS {
 
@@ -11,18 +12,18 @@ public class PreSenseWS {
 	// TO-DO: usar lib json para leitura da entrada
 	// TO-DO: lidar com a autentificação (pra extrair evento)
 	public void receiveInfoFromSensor(byte[] resource) {
-
+		// String res = new String(byte);
 		// // < Fazer leitura Json>
+		// Gson gson = new Gson();
+		// Receiver recv = gson.fromJson(json, Receiver.class);
 		// if (/*HTTP GET*/) {
 		// 	this.auth(/*stream*/);
 		// 	return;
 		// }
 		// // else
 		
-		// String bId = /*getMajor + getMinor + getUuid*/;
-		// String res[] = /*getTime*/.split("[-] | [T] | [:] | [Z]");
-		// String user = /*getUser*/
-		// String pass = /*getPass*/
+		// String bId = recv.getMajor + recv.getMinor + recv.getUuid;
+		// String t[] = recv.getTime.split("[-] | [T] | [:] | [Z]");
 
 		// // Convert time to Timestamp 
 		// Calendar cal = Calendar.getInstance(); 
@@ -32,22 +33,22 @@ public class PreSenseWS {
 		
 		// // Retrieve event using sensor's search
 		// Event ev = EventDAO.findEventById(
-		// 	/*SensorDao.findSensorByName(user, pass).getEvent());*/
+		// 	/*SensorDao.findSensorByName(recv.getUser, recv.getPass).getEvent())*/;
 		// if (ev == null) {
 		// 	System.err.println("Unregistered event")
 		// 	return;
 		// }
 		
 		// // Retrieve person using beacon's search
-		// Person p = BeaconDAO.findBeaconById().getPerson();
+		// Person p = BeaconDAO.findBeaconById(bId).getPerson();
 		// if (p == null) {
 		// 	System.err.println("Unregistered person")
 		// 	return;
 		// }
 				
 		// // Search for Attendance, if doesn't exist, creat it
-		// Attendance att = findAttendanceByEventAndPerson(e, p);
-		// if (att == null) att = new Attendance(e, p, new Timestamp(0), new Timestamp(time));
+		// Attendance att = findAttendanceByEventAndPerson(ev, p);
+		// if (att == null) att = new Attendance(ev, p, new Timestamp(0), new Timestamp(time));
 		// else {
 		// 	// Process rightful sensor-event type
 		// 	if (type.equals("in")) att.setLastTime(time);

675343886ab67bde7207420ff634e54c1a8112d8
Vítor Tonon <vitorrtonon@gmail.com>
2015-11-09 14:34:21 -0200

    teste

--- src/main/java/topcom/presense/server/services/PreSenseWS.java
+++ src/main/java/topcom/presense/server/services/PreSenseWS.java
@@ -4,7 +4,6 @@ import topcom.presense.server.dao.*;
 import topcom.presense.server.pojo.*;
 import java.util.Calendar;
 import java.sql.Timestamp;
-import com.google.gson.Gson;
 
 public class PreSenseWS {
 

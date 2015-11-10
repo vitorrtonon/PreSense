4f7f6cfb4758176b5569c94698cfc99038b2e5d7
Vítor Tonon <vitorrtonon@gmail.com>
2015-11-10 11:14:24 -0200

    Método de busca do sensor por nome e passcode

--- src/main/java/topcom/presense/server/dao/SensorDAO.java
+++ src/main/java/topcom/presense/server/dao/SensorDAO.java
@@ -9,6 +9,7 @@ public class SensorDAO extends HibernateDAO {
 
 	private static final String SELECT_SENSOR_BY_ID = "from Sensor S where S.id = :id";
 	private static final String SELECT_SENSOR_BY_PIN = "from Sensor S where S.pin = :pin";
+	private static final String SELECT_SENSOR_BY_NAME_AND_PASS = "from Sensor S where S.name = :name and S.pass = :pass";
 
 	public boolean insert(Sensor obj) {
 
@@ -72,6 +73,40 @@ public class SensorDAO extends HibernateDAO {
 
 			Query query = session.createQuery(SELECT_SENSOR_BY_PIN);
 			query.setInteger("pin", pin);
+
+			sensor = (Sensor) query.uniqueResult();
+
+			transaction.commit();
+		}
+		catch(Exception e) {
+
+			if(transaction != null) transaction.rollback();
+
+			e.printStackTrace();
+		}
+		finally {
+
+			if(session != null) session.close();
+		}
+	
+		return sensor;
+	}
+
+	public Sensor findSensorByNameAndPass(String name, String pass) {
+
+		Session session = null;
+		Transaction transaction = null;
+		Sensor sensor = null;
+
+		try {
+
+			session = HibernateUtil.getSessionFactory().openSession();
+
+			transaction = session.beginTransaction();
+
+			Query query = session.createQuery(SELECT_SENSOR_BY_NAME_AND_PASS);
+			query.setString("name", name);
+			query.setString("pass", pass);
 
 			sensor = (Sensor) query.uniqueResult();
 

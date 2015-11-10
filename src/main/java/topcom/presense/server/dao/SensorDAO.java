2caf604ec539c42fbd1aedcc033f6cbb52670729
Vítor Tonon <vitorrtonon@gmail.com>
2015-11-10 09:39:42 -0200

    Campo pin e passcode adicionados ao Sensor

--- src/main/java/topcom/presense/server/dao/SensorDAO.java
+++ src/main/java/topcom/presense/server/dao/SensorDAO.java
@@ -8,6 +8,7 @@ import topcom.presense.server.util.*;
 public class SensorDAO extends HibernateDAO {
 
 	private static final String SELECT_SENSOR_BY_ID = "from Sensor S where S.id = :id";
+	private static final String SELECT_SENSOR_BY_PIN = "from Sensor S where S.pin = :pin";
 
 	public boolean insert(Sensor obj) {
 
@@ -38,6 +39,39 @@ public class SensorDAO extends HibernateDAO {
 
 			Query query = session.createQuery(SELECT_SENSOR_BY_ID);
 			query.setLong("id", id);
+
+			sensor = (Sensor) query.uniqueResult();
+
+			transaction.commit();
+		}
+		catch(Exception e) {
+
+			if(transaction != null) transaction.rollback();
+
+			e.printStackTrace();
+		}
+		finally {
+
+			if(session != null) session.close();
+		}
+	
+		return sensor;
+	}
+
+	public Sensor findSensorByPin(int pin) {
+
+		Session session = null;
+		Transaction transaction = null;
+		Sensor sensor = null;
+
+		try {
+
+			session = HibernateUtil.getSessionFactory().openSession();
+
+			transaction = session.beginTransaction();
+
+			Query query = session.createQuery(SELECT_SENSOR_BY_PIN);
+			query.setInteger("pin", pin);
 
 			sensor = (Sensor) query.uniqueResult();
 
--- src/main/java/topcom/presense/server/pojo/Sensor.java
+++ src/main/java/topcom/presense/server/pojo/Sensor.java
@@ -3,21 +3,27 @@ package topcom.presense.server.pojo;
 public class Sensor {
 
 	private long id;
+	private int pin;
+	private String passcode;
 	private String name;
 	private Event event;
 
 	public Sensor() {}
 
-	public Sensor(long id, String name) {
+	public Sensor(long id, int pin, String passcode, String name) {
 
 		this.setId(id);
+		this.setPin(pin);
+		this.setPasscode(passcode);
 		this.setName(name);
 		this.setEvent(null);
 	}
 
-	public Sensor(long id, String name, Event event) {
+	public Sensor(long id, int pin, String passcode, String name, Event event) {
 
 		this.setId(id);
+		this.setPin(pin);
+		this.setPasscode(passcode);
 		this.setName(name);
 		this.setEvent(event);
 	}
@@ -28,6 +34,23 @@ public class Sensor {
 
 	public long getId() {
 		return this.id;
+	}
+
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+
+	public int getPin() {
+
+		return this.pin;
+	}
+
+	public void setPasscode(String passcode) {
+		this.passcode = passcode;
+	}
+
+	public String getPasscode() {
+		return this.passcode;
 	}
 	
 	public void setName(String name) {
